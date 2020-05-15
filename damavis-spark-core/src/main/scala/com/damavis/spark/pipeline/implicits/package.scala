c0e8dec625e3a7687f82f4dce581fc02d279b145
priera <mail@rieramartorell.cat>
2020-05-15 15:51:36 +0200

    Added implicits

---
+++ damavis-spark-core/src/main/scala/com/damavis/spark/pipeline/implicits/package.scala
@@ -0,0 +1,18 @@
+package com.damavis.spark.pipeline
+
+import scala.language.implicitConversions
+import com.damavis.spark.resource.{ResourceReader, ResourceWriter}
+import org.apache.spark.sql.DataFrame
+
+package object implicits {
+
+  implicit def readerToSource(resource: ResourceReader): PipelineSource =
+    new PipelineSource {
+      override def get: DataFrame = resource.read()
+    }
+
+  implicit def writerToTarget(resource: ResourceWriter): PipelineTarget =
+    new PipelineTarget {
+      override def put(data: DataFrame): Unit = resource.write(data)
+    }
+}
