d1c67631b9459441b5de5b4027d302366a24e5ce
priera <pedro.riera@damavis.com>
2020-05-21 17:31:13 +0200

    next test

---
+++ damavis-spark-core/src/test/scala/com/damavis/spark/fs/HadoopFSTest.scala
@@ -0,0 +1,28 @@
+package com.damavis.spark.fs
+
+import com.damavis.spark.entities.Person
+import com.damavis.spark.utils.SparkTestSupport
+
+class HadoopFSTest extends SparkTestSupport {
+
+  import spark.implicits._
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+
+    (Person("person1", 23) :: Person("person2", 346) :: Nil)
+      .toDF()
+      .write
+      .format("parquet")
+      .save("/persons")
+  }
+
+  "A Hadoop FS" should {
+    "tell whether a path exists or not" in {
+      val fs = HadoopFS()
+
+      assert(fs.pathExists("/persons"))
+      assert(!fs.pathExists("/persons2"))
+    }
+  }
+}
