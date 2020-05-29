54ac96fa60aaa95cad3955c08a155fbf18baa075
priera <pedro.riera@damavis.com>
2020-05-29 13:21:20 +0200

    table writer tests fixed

--- damavis-spark-core/src/test/scala/com/damavis/spark/resource/datasource/TableResourceWriterTest.scala
+++ damavis-spark-core/src/test/scala/com/damavis/spark/resource/datasource/TableResourceWriterTest.scala
@@ -3,18 +3,14 @@ package com.damavis.spark.resource.datasource
 import java.util.concurrent.atomic.AtomicInteger
 
 import com.damavis.spark.database.{Database, DbManager, Table}
-import com.damavis.spark.resource.datasource.enums.{
-  Format,
-  OverwritePartitionBehavior
-}
+import com.damavis.spark.resource.datasource.enums.OverwritePartitionBehavior
 import com.damavis.spark.utils.SparkTestSupport
 import org.apache.spark.sql.SaveMode
-import org.apache.spark.sql.types._
 import com.damavis.spark._
 
 class TableResourceWriterTest extends SparkTestSupport {
 
-  var db: Database = _
+  implicit var db: Database = _
   val tableCount = new AtomicInteger(0)
 
   override def beforeAll(): Unit = {
@@ -23,16 +19,8 @@ class TableResourceWriterTest extends SparkTestSupport {
     db = DbManager.useDatabase(name, forceCreation = true)
   }
 
-  private def preparePersonsTable(partitionCols: Seq[String]): Table = {
-    val schema = StructType(
-      StructField("name", StringType) ::
-        StructField("age", IntegerType) ::
-        StructField("nationality", StringType) ::
-        Nil
-    )
-    val tableName = s"persons${tableCount.addAndGet(1)}"
-
-    //db.prepareTable(tableName, Format.Parquet, schema, partitionCols)
+  private def nextTable: Table = {
+    val tableName = s"authors${tableCount.addAndGet(1)}"
 
     val tryTable = db.getTable(tableName)
 
@@ -41,165 +29,161 @@ class TableResourceWriterTest extends SparkTestSupport {
     tryTable.get
   }
 
-  private def prepareNotPartitionedTable(): Table = preparePersonsTable(Nil)
-  private def preparePartitionedTable(): Table =
-    preparePersonsTable("nationality" :: Nil)
-
-  /*"A TableResourceWriter" when {
+  "A TableResourceWriter" when {
     "there is no partitioning" should {
       "write successfully to an empty table" in {
-        val table = prepareNotPartitionedTable()
+        val table = nextTable
         val writer = TableWriterBuilder(table).writer()
 
-        val personDf = dfFromAuthors(orson)
+        val personDf = dfFromAuthors(hemingway)
         writer.write(personDf)
 
-        val written = session.read.parquet(table.path)
+        val written = session.read.table(table.name)
 
         assert(written.count() == 1)
-        assert(written.except(personDf).isEmpty)
+        checkDataFramesEqual(written, personDf)
       }
 
       "apply properly append save mode" in {
-        val table = prepareNotPartitionedTable()
+        val table = nextTable
         val writerBuilder = TableWriterBuilder(table)
 
-        val person1 = dfFromAuthors(orson)
+        val author1 = dfFromAuthors(hemingway)
         writerBuilder
           .writer()
-          .write(person1)
+          .write(author1)
 
-        val person2 = dfFromAuthors(wells)
+        val author2 = dfFromAuthors(wells)
         writerBuilder
           .saveMode(SaveMode.Append)
           .writer()
-          .write(person2)
+          .write(author2)
 
-        val written = session.read.parquet(table.path)
+        val written = session.read.table(table.name)
         assert(written.count() == 2)
 
-        val expectedDf = dfFromAuthors(orson, wells)
-        assert(written.except(expectedDf).isEmpty)
+        val expectedDf = dfFromAuthors(hemingway, wells)
+        checkDataFramesEqual(written, expectedDf)
       }
 
       "apply properly overwrite save mode" in {
-        val table = prepareNotPartitionedTable()
+        val table = nextTable
         val writerBuilder = TableWriterBuilder(table)
 
-        val person1 = dfFromAuthors(orson)
+        val author1 = dfFromAuthors(hemingway)
         writerBuilder
           .writer()
-          .write(person1)
+          .write(author1)
 
-        val person2 = dfFromAuthors(wells)
+        val author2 = dfFromAuthors(wells)
         writerBuilder
           .saveMode(SaveMode.Overwrite)
           .writer()
-          .write(person2)
+          .write(author2)
 
-        val written = session.read.parquet(table.path)
+        val written = session.read.table(table.name)
 
         assert(written.count() == 1)
-        assert(written.except(person2).isEmpty)
+        checkDataFramesEqual(written, author2)
       }
     }
 
     "there is partitioning" should {
       "write successfully to an empty table" in {
-        val table = preparePartitionedTable()
+        val table = nextTable
         val writer = TableWriterBuilder(table)
           .partitionedBy("nationality")
           .writer()
 
-        val personDf = dfFromAuthors(orson)
+        val personDf = dfFromAuthors(hemingway)
         writer.write(personDf)
 
-        val written = session.read.parquet(table.path)
+        val written = session.read.table(table.name)
 
         assert(written.count() == 1)
-        assert(written.except(personDf).isEmpty)
+        checkDataFramesEqual(written, personDf)
       }
 
       "overwrite all partitions by default" in {
-        val table = preparePartitionedTable()
+        val table = nextTable
         val writer = TableWriterBuilder(table)
           .partitionedBy("nationality")
           .writer()
 
-        val authors = dfFromAuthors(orson, wells)
+        val authors = dfFromAuthors(hemingway, wells)
         writer.write(authors)
 
         val anotherUSAAuthor = dfFromAuthors(bradbury)
         writer.write(anotherUSAAuthor)
 
-        val finalDf = session.read.parquet(table.path)
+        val finalDf = session.read.table(table.name)
 
         assert(finalDf.count() == 1)
-        assert(finalDf.except(anotherUSAAuthor).count() == 0)
+        checkDataFramesEqual(finalDf, anotherUSAAuthor)
       }
 
       "overwrite all partitions if told so" in {
-        val table = preparePartitionedTable()
+        val table = nextTable
         val writer = TableWriterBuilder(table)
           .partitionedBy("nationality")
           .overwritePartitionBehavior(OverwritePartitionBehavior.OVERWRITE_ALL)
           .writer()
 
-        val authors = dfFromAuthors(orson, wells)
+        val authors = dfFromAuthors(hemingway, wells)
         writer.write(authors)
 
         val anotherUSAAuthor = dfFromAuthors(bradbury)
         writer.write(anotherUSAAuthor)
 
-        val finalDf = session.read.parquet(table.path)
+        val finalDf = session.read.table(table.name)
 
         assert(finalDf.count() == 1)
-        assert(finalDf.except(anotherUSAAuthor).count() == 0)
+        checkDataFramesEqual(finalDf, anotherUSAAuthor)
       }
 
       "overwrite only matching partitions if parameter is set" in {
-        val table = preparePartitionedTable()
+        val table = nextTable
         val writer = TableWriterBuilder(table)
           .partitionedBy("nationality")
           .overwritePartitionBehavior(
             OverwritePartitionBehavior.OVERWRITE_MATCHING)
           .writer()
 
-        val authors = dfFromAuthors(orson, wells)
+        val authors = dfFromAuthors(hemingway, wells)
         writer.write(authors)
 
         val anotherUSAAuthor = dfFromAuthors(bradbury)
         writer.write(anotherUSAAuthor)
 
-        val finalDf = session.read.parquet(table.path)
+        val finalDf = session.read.table(table.name)
 
         val expectedAuthors = dfFromAuthors(bradbury, wells)
 
         assert(finalDf.count() == 2)
-        assert(finalDf.except(expectedAuthors).count() == 0)
+        checkDataFramesEqual(finalDf, expectedAuthors)
       }
 
       "do not overwrite anything if save mode is different than overwrite" in {
-        val table = preparePartitionedTable()
+        val table = nextTable
         val writer = TableWriterBuilder(table)
           .partitionedBy("nationality")
           .saveMode(SaveMode.Append)
           .writer()
 
-        val authors = dfFromAuthors(orson, wells)
+        val authors = dfFromAuthors(hemingway, wells)
         writer.write(authors)
 
         val anotherUSAAuthor = dfFromAuthors(bradbury)
         writer.write(anotherUSAAuthor)
 
-        val finalDf = session.read.parquet(table.path)
+        val finalDf = session.read.table(table.name)
 
-        val expectedAuthors = dfFromAuthors(orson, wells, bradbury)
+        val expectedAuthors = dfFromAuthors(hemingway, wells, bradbury)
 
         assert(finalDf.count() == 3)
-        assert(finalDf.except(expectedAuthors).count() == 0)
+        checkDataFramesEqual(finalDf, expectedAuthors)
       }
     }
-  } */
+  }
 
 }
