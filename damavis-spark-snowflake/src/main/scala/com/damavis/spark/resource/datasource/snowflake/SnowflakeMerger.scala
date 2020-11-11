46cbcf74d732e8c262bfd1047c70801150a79217
Cristòfol Torrens <piffall@gmail.com>
2020-11-11 12:33:44 +0100

    Adding experimental support for Snowflake.

---
+++ damavis-spark-snowflake/src/main/scala/com/damavis/spark/resource/datasource/snowflake/SnowflakeMerger.scala
@@ -0,0 +1,47 @@
+package com.damavis.spark.resource.datasource.snowflake
+
+import net.snowflake.spark.snowflake.Utils
+import org.apache.spark.sql.SparkSession
+
+case class SnowflakeMerger(
+    account: String,
+    user: String,
+    password: String,
+    warehouse: String,
+    database: String,
+    schema: String,
+    sourceTable: String,
+    targetTable: String,
+    pkColumns: Seq[String])(implicit spark: SparkSession) {
+
+  val sfOptions = Map(
+    "sfURL" -> s"${account}.snowflakecomputing.com",
+    "sfUser" -> user,
+    "sfPassword" -> password,
+    "sfDatabase" -> database,
+    "sfSchema" -> schema,
+    "sfWarehouse" -> warehouse,
+    "sfCompress" -> "on",
+    "sfSSL" -> "on"
+  )
+
+  private def mergeExpression(pkColumns: Seq[String]): String = {
+    pkColumns
+      .zip(pkColumns)
+      .map(tuple => s"${targetTable}.${tuple._1} = ${sourceTable}.${tuple._2}")
+      .mkString(" AND ")
+  }
+
+  def merge(): Unit = {
+    val deleteQuery =
+      s"""
+        |MERGE INTO ${targetTable} USING ${sourceTable}
+        |ON ${mergeExpression(pkColumns)}
+        |WHEN MATCHED THEN DELETE
+        |""".stripMargin
+
+    Utils.runQuery(sfOptions, deleteQuery)
+    println("deleted")
+  }
+
+}
---
+++ damavis-spark-snowflake/src/main/scala/com/damavis/spark/resource/datasource/snowflake/SnowflakeWriterMerger.scala
@@ -0,0 +1,56 @@
+package com.damavis.spark.resource.datasource.snowflake
+
+import com.damavis.spark.resource.ResourceWriter
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
+
+case class SnowflakeWriterMerger(
+    writer: SnowflakeWriter,
+    data: DataFrame,
+    columns: Seq[String])(implicit spark: SparkSession)
+    extends ResourceWriter {
+
+  val stagingTable = s"merge_tmp_delta__${writer.table}"
+  val targetTable = s"${writer.table}"
+
+  override def write(data: DataFrame): Unit = {
+    if (targetExists()) {
+      merge()
+      writer.copy(mode = SaveMode.Append).write(data)
+    } else {
+      writer.copy(mode = SaveMode.Overwrite).write(data)
+    }
+  }
+
+  private def merge(): Unit = {
+    writer.copy(table = stagingTable, mode = SaveMode.Overwrite).write(data)
+    SnowflakeMerger(writer.account,
+                    writer.user,
+                    writer.password,
+                    writer.warehouse,
+                    writer.database,
+                    writer.schema,
+                    stagingTable,
+                    targetTable,
+                    columns).merge()
+  }
+
+  private def targetExists(): Boolean = {
+    val reader = SnowflakeReader(
+      writer.account,
+      writer.user,
+      writer.password,
+      writer.warehouse,
+      writer.database,
+      "INFORMATION_SCHEMA",
+      query = Some(
+        s"SELECT COUNT(1) = 1 FROM TABLES WHERE TABLE_NAME = '${targetTable}'")
+    )
+
+    reader
+      .read()
+      .collect
+      .map(_.getBoolean(0))
+      .head
+  }
+
+}
